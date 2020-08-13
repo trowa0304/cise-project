5df58a97f03d4679d97b65f7c73b6f8735ff3350
ducng99 <tuanduc249.td@gmail.com>
2020-08-13 05:43:26 +0000

    Added Mocha and Chai as testing framework Also added babel-register and its plugins to work with Mocha

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import {expect} from "chai";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("a test", () =>
+{
+    it("test array", () =>
+    {
+        let array = [];
+        
+        expect(array.length).to.equal(0);
+    });
 });
