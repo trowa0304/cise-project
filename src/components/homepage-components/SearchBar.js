2de6ef2860d1b26a21756e9c449f2e899d434d46
trowa0304 <shichaom@gmail.com>
2020-09-03 23:28:21 +1200

    optim page layout

--- src/components/homepage-components/SearchBar.js
+++ src/components/homepage-components/SearchBar.js
@@ -5,7 +5,7 @@ class SearchBar extends React.Component {
   render() {
     return (
       <div>
-        <h1 id="heading">SEER</h1>
+        <h1 className="foo" id="heading">SEER</h1>
 
         <input type="text" placeholder="Search" name="Search" className="extendedbar"></input>
       </div>
