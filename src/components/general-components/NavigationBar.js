82c83b07b527ee1a731598bf8daf60d68311a5ac
nigelph <54135755+nigelph@users.noreply.github.com>
2020-09-03 20:43:52 +1200

    Tangible home page
    -Created a prototype of the home page sketch

---
+++ src/components/general-components/NavigationBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./NavigationBar.css";
+import { Navbar, Nav } from "react-bootstrap";
+
+class NavigationBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">SEER</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Submit New Evidence</Nav.Link>
+          </Nav>
+        </Navbar>
+      </div>
+    );
+  }
+}
+export default NavigationBar;
---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import SearchBar from "../homepage-components/SearchBar";
+import SearchFilter from "../homepage-components/SearchFilter";
+import SearchButton from "../homepage-components/SearchButton";
+import { Container } from "react-bootstrap";
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+       
+          <SearchBar />
+          <SearchFilter />
+          <SearchButton />
+    
+      </div>
+    );
+  }
+}
+export default HomePage;
