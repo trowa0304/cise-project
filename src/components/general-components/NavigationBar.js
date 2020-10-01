02ec5c88a98e1046bb3fbb4d2a4c98d14ab486be
nigelph <54135755+nigelph@users.noreply.github.com>
2020-10-01 13:12:36 +1300

    NavigationBar conflict issues
    Fixed NavigationBar.js conflict issues

--- src/components/general-components/NavigationBar.js
+++ src/components/general-components/NavigationBar.js
@@ -7,14 +7,10 @@ class NavigationBar extends React.Component {
     return (
       <div>
         <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="/">SEER</Navbar.Brand>
+        <Navbar.Brand href="/">SEER</Navbar.Brand>
           <Nav className="mr-auto">
-            <Nav.Link href="#">Home</Nav.Link>
-            <Nav.Link href="#features">Submit New Evidence</Nav.Link>
-            <Nav.Link pullRight href='#LogIn'style={{justifyContent: 'right'}} activeStyle={{fontWeight: 'bold', color: 'red'}}>Log in/Sign up</Nav.Link>
-            <h1>REMOVE ME AFTER</h1>
-
             <Nav.Link href="/submit">Submit New Evidence</Nav.Link>
+            <Nav.Link pullRight href='#LogIn'style={{justifyContent: 'right'}} activeStyle={{fontWeight: 'bold', color: 'red'}}>Log in/Sign up</Nav.Link>
           </Nav>
         </Navbar>
       </div>
